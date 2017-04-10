da90deeb937537b8aebf8e7cc5160a99eaba565e
eqs <tikuwatabetai@gmail.com>
2017-04-10 14:55:34 +0900

    implement function that label class to data

--- main.py
+++ main.py
@@ -14,11 +14,28 @@ from PyQt5.QtWidgets import (QMainWindow, QApplication, QWidget, QGridLayout,
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtCore 
 
+BUTTON_CONFIG = [
+    {
+         'key' : 'A', 
+         'class' : 'dipping_gestures'
+    }, 
+    {
+         'key' : 'S', 
+         'class' : 'other_gestures'
+    },
+    {
+         'key' : 'D',
+         'class' : 'other_things'
+    }
+]
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         
-        # Initialize main-widget
+        # Initialize main-widgets
+        
+        # 画像をスライドするボタンの設定
         self.prevButton = QPushButton('&Prev')
         self.prevButton.clicked.connect(self.showPrevImage)
         self.prevButton.setShortcut('H')
@@ -31,12 +48,32 @@ class MainWindow(QMainWindow):
         buttonLayout.addWidget(self.prevButton, 0, 0)
         buttonLayout.addWidget(self.nextButton, 0, 1)
         
+        # 画像を表示するラベルの設定
         self.pictureLabel = QLabel()
         self.pictureLabel.setAlignment(QtCore.Qt.AlignCenter)
         self.putPixmap('usagi.jpg')
         
+        self.pathLabel = QLabel('')
+        self.pathLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.classLabel = QLabel('')
+        self.classLabel.setAlignment(QtCore.Qt.AlignCenter)
+        
+        pictureLayout = QGridLayout()
+        pictureLayout.addWidget(self.pictureLabel, 0, 0)
+        pictureLayout.addWidget(self.pathLabel, 1, 0)
+        pictureLayout.addWidget(self.classLabel, 2, 0)
+        
+        # アノテーションをするためのボタンの設定
+        annotationButtonLayout = QGridLayout()
+        for k, conf in enumerate(BUTTON_CONFIG):
+            button = QPushButton('&{0} : {1}'.format(conf['class'], conf['key']))
+            button.clicked.connect(self.anotateClass)
+            button.setShortcut(conf['key'])
+            annotationButtonLayout.addWidget(button, 0, k)
+        
         mainLayout = QVBoxLayout()
-        mainLayout.addWidget(self.pictureLabel)
+        mainLayout.addLayout(pictureLayout)
+        mainLayout.addLayout(annotationButtonLayout)
         mainLayout.addLayout(buttonLayout)
         
         mainWidget = QWidget()
@@ -78,15 +115,30 @@ class MainWindow(QMainWindow):
         self.pictureLabel.setPixmap(pixmap.scaled(256, 256, QtCore.Qt.KeepAspectRatio))
         self.pictureLabel.show()
     
+    def anotateClass(self):
+        # 押されたボタンの情報から，ラベリングを行う
+        text = self.sender().text()
+        dataClass = text[1:text.index(':')-1].strip()
+        self.imageDataList[self.imageIndex]['class'] = dataClass
+        self.updateDataInformation()
+    
+    def updateDataInformation(self):
+        # 現在選択しているデータのパスとクラスをラベルに表示する
+        if len(self.imageDataList) > 0:
+            self.pathLabel.setText(self.imageDataList[self.imageIndex]['filepath'])
+            self.classLabel.setText(self.imageDataList[self.imageIndex]['class'])
+    
     def showPrevImage(self):
         if len(self.imageDataList) > 0:
             self.imageIndex = (self.imageIndex - 1) % len(self.imageDataList)
             self.putPixmap(self.imageDataList[self.imageIndex]['filepath'])
+            self.updateDataInformation()
     
     def showNextImage(self):
         if len(self.imageDataList) > 0:
             self.imageIndex = (self.imageIndex + 1) % len(self.imageDataList)
             self.putPixmap(self.imageDataList[self.imageIndex]['filepath'])
+            self.updateDataInformation()
         
     def openAnnotationFile(self):
         QFileDialog.getOpenFileName(parent=self, filter='*.json')
@@ -104,12 +156,13 @@ class MainWindow(QMainWindow):
         for imagePath in selectedImagePathList:
             # まだ追加されていない画像ならリストに追加する
             if not (imagePath in imagePathList):
-                self.imageDataList.append({'filepath' : imagePath, 'annotation' : None})
+                self.imageDataList.append({'filepath' : imagePath, 'class' : None})
         
         # 画像があるなら開く
         if len(self.imageDataList) > 0:
             self.imageIndex = len(self.imageDataList) - 1
             self.putPixmap(self.imageDataList[self.imageIndex]['filepath'])
+            self.updateDataInformation()
             
         
 if __name__ == '__main__':
